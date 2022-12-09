e6eeac8bbb39c01a26cc9cc176ff0c1e57ecec55
dobbse42 <edjunk13@hotmail.com>
2022-12-09 02:15:08 -0500

    Mod2sparse stuff finished mostly, remaining todo: possible bugs/syntax errors, fix bp_decode_log_prob_ratios, optimize. Added bp_decode_prob_ratios. Use bp_decode_2.jl.

--- src/bp_decode_2.jl
+++ src/bp_decode_2.jl
@@ -9,8 +9,9 @@
 # Possible options:
 #   1. Two parallel sparse matrices, one for bit_to_check and one for check_to_bit.
 #   2. Two arrays parallel with m.nzval of bit_to_check messages and check_to_bit messages.
-
+# A: Currently going with option 2, called bits_to_checks and checks_to_bits respectively.
 using BitSAD
+using SparseArrays
 
 #DONE:
 
@@ -44,6 +45,8 @@ end
 
 #TODO:
 
+
+#structs, SBitStreams
 
 
 #MOSTLY DONE:
@@ -95,22 +98,116 @@ function mod2sparse_last_in_row(m, i)
 end
 
 function mod2sparse_at_start_col(m, j, e)
-    return (m.colptr[j] >= e)
+    return (m.colptr[j] <= e)
+end
+
+function mod2sparse_prev_in_col(e)
+    return e-1
+end
+
+function mod2sparse_mulvec(H, received_codeword, synd)
+    #rows is [1], cols is [2]
+    M = size(H)[1]
+    N = size(H)[2]
+
+    for i in 1:M
+        synd[i] = 0
+    end
+
+    for j in 1:N
+        if received_codeword[j] == 1
+            e = mod2sparse_first_in_col(H, j)
+            while !(mod2sparse_at_end_col(H, j, e)
+                synd[H.rowval[e]] ^= 1
+                e = mod2sparse_next_in_col(e)
+            end
+        end
+    end
+end
+
+function bp_decode_prob_ratios(dec) #product-sum
+    for j in 1:dec.N
+        # e is first non-zero entry in column j of H.
+        e = mod2sparse_first_in_col(dec.H, j)
+        while !(mod2sparse_at_end_col(m, j, e))
+            dec.bits_to_checks[e] = SBitstream(float(dec.channel_probs[j]) / (1.0 - float(dec.channel_probs[j]) #first
+            e = mod2sparse_next_in_col(e)
+        end
+
+    dec.converge = 0
+
+    #TODO: change this to go by columns rather than by rows, Julia is column-major.
+    for iteration in 1:(dec.max_iter+1)
+        for i in 1:dec.M
+            e = mod2sparse_first_in_row(dec.H, i)
+            temp = SBitstream((-1.0) ^(dec.synd[i]))
+            while !(mod2sparse_at_end_row(dec.H, i, e)
+                checks_to_bits[e] = temp #first
+                temp = temp * SBitstream(2.0/(1.0 + float(bits_to_checks(e)) - 1))
+                e = mod2sparse_next_in_row(dec.H, e)
+            end
+            e = mod2sparse_last_in_row(dec.H, i)
+            temp = SBitstream(1.0)
+            while !(mod2sparse_at_start_row(dec.H, i, e)
+                checks_to_bits[e] = checks_to_bits[e] * temp
+                checks_to_bits[e] = SBitstream((1.0 - float(checks_to_bits[e])) / (1.0 + float(checks_to_bits[e])))
+                temp = temp * SBitstream(2.0 / (1.0 + float(bits_to_checks[e])) - 1.0)
+                e = mod2sparse_prev_in_row(dec.H, e)
+            end
+
+        #bit to check messages
+        for j in 1:dec.N
+            e = mod2sparse_first_in_col(dec.H, j)
+            temp = SBitstream(float(dec.channel_probs[j]) / (1.0 - float(dec.channel_probs[j])))
+            while !(mod2sparse_at_end_col(dec.H, j, e)
+                bits_to_checks[e] = temp
+                temp = temp * checks_to_bits[e]
+                #Maybe an isnan(temp) check here? idk when that would be true though. bp_decoder.pyx line 287
+                e = mod2sparse_next_in_col(e)
+            end
+            #dec.log_prob_ratios[j] = temp
+            if float(temp) >= 1 #????? How? Should this not be <= 1?
+                dec.bp_decoding[j] = 1
+            else
+                dec.bp_decoding[j] = 0
+            end
+
+            e = mod2sparse_last_in_col(dec.H, j)
+            temp = SBitstream(1.0)
+            while !(mod2sparse_at_start_col)
+                bits_to_checks[e] = bits_to_checks[e] * temp
+                temp = temp * checks_to_bits[e]
+                #maybe nan check on temp again?
+                e = mod2sparse_prev_in_col(e)
+            end
+
+            mod2sparse_mulvec(dec.H, dec.bp_decoding, dec.bp_decoding_synd) #no SBitstreams!!
+
+            equal = 1
+            for check in 1:dec.M
+                if dec.synd[check] != dec.bp_decoding_synd[check]
+                    equal = 0
+                    break
+                end
+            end
+            if equal == 1
+                dec.converge = 1
+                return 1
+            end
+
+            return 0
 end
 
 #IN PROGRESS:
 
-function mod2sparse_prev_in_col()
-
-end
-
-function bp_decode_log_prob_ratios(dec)
+function bp_decode_log_prob_ratios(dec) #product-sum
     for j in 1:dec.N
         # e is first non-zero entry in column j of H.
         e = mod2sparse_first_in_col(dec.H, j)
-        while !(mod2sparse_at_end(m, j, e))
+        while !(mod2sparse_at_end_col(m, j, e))
             dec.bits_to_checks[e] = log((1 - dec.channel_probs[j]) / (dec.channel_probs[j]))
             e = mod2sparse_next_in_col(e)
+        end
 
     dec.converge = 0
     #TODO: change this to go by columns rather than by rows, Julia is column-major.
@@ -119,9 +216,10 @@ function bp_decode_log_prob_ratios(dec)
             e = mod2sparse_first_in_row(dec.H, i)
             temp = 1.0
             while !(mod2sparse_at_end_row(dec.H, i, e)
-                checks_to_bits[e] = temp
+                checks_to_bits[e] = temp #first
                 temp *= tanh(bits_to_checks[e] / 2)
                 e = mod2sparse_next_in_row(dec.H, e)
+            end
             e = mod2sparse_last_in_row(m, i)
             temp = 1.0
             while !(mod2sparse_at_start_row(dec.H, i, e)
@@ -129,6 +227,7 @@ function bp_decode_log_prob_ratios(dec)
                 checks_to_bits[e] = (((-1)^dec.synd[i]) * log((1 + checks_to_bits[e]) / (1 - checks_to_bits[e])))
                 temp *= tanh(bits_to_checks[e] / 2)
                 e = mod2sparse_prev_in_row(dec.H, e)
+            end
 
         #bit to check messages
         for j in 1:dec.N
@@ -138,6 +237,7 @@ function bp_decode_log_prob_ratios(dec)
                 bits_to_checks[e] = temp
                 temp += checks_to_bits[e]
                 e = mod2sparse_next_in_col(e)
+            end
             dec.log_prob_ratios[j] = temp
             if temp <= 0
                 dec.bp_decoding[j] = 1
@@ -150,4 +250,36 @@ function bp_decode_log_prob_ratios(dec)
                 bits_to_checks[e] += temp
                 temp += checks_to_bits[e]
                 e = mod2sparse_prev_in_col(e)
+            end
+
+            mod2sparse_mulvec(dec.H, dec.bp_decoding, dec.bp_decoding_synd)
+
+            equal = 1
+            for check in 1:dec.M
+                if dec.synd[check] != dec.bp_decoding_synd[check]
+                    equal = 0
+                    break
+                end
+            end
+            if equal == 1
+                dec.converge = 1
+                return 1
+            end
+
+            return 0
+end
+
+struct decoder()
+    channel_probs #SBitstream[]
+    H
+    max_iter
+    synd
+    log_prob_ratios #SBitstream[]
+    bp_decoding
+    bp_decoding_synd
+    N
+    M
+    converge
+    bits_to_checks #SBitstream[]
+    checks_to_bits #SBitstream[]
 end
